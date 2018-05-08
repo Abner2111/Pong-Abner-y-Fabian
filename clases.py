fdb1f5cc8404bc78da3510df6f6af4102d23a813
Abner2111 <abneraq73@gmail.com>
2018-05-07 19:41:54 -0600

    Performance mejorado

--- clases.py
+++ clases.py
@@ -6,12 +6,13 @@ def main():
     pygame.display.set_mode([800, 400])
     pygame.display.set_caption("Pong")
     salir =  False
+    reloj1 = pygame.time.Clock()
 
     while not salir:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 salir = True
-
+        reloj1.tick(20)
         pygame.display.update()
     pygame.quit()
 
