b29df4b0492a7211fce4b6ce238ed2bdda547be0
Abner2111 <abneraq73@gmail.com>
2018-05-07 19:36:57 -0600

    Agreguue ventana

--- clases.py
+++ clases.py
@@ -1,4 +1,19 @@
 import pygame
 
+def main():
+    pygame.init()
+
+    pygame.display.set_mode([800, 400])
+    pygame.display.set_caption("Pong")
+    salir =  False
+
+    while not salir:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                salir = True
+
+        pygame.display.update()
+    pygame.quit()
 
 
+main()
