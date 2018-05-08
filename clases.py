6d58efd0022bfd031649ca95c8f87f56f50aae53
Abner2111 <abneraq73@gmail.com>
2018-05-07 21:22:00 -0600

    pasamos a tkinter

--- clases.py
+++ clases.py
@@ -1,20 +1,3 @@
-import pygame
-
-def main():
-    pygame.init()
-
-    pygame.display.set_mode([800, 400])
-    pygame.display.set_caption("Pong")
-    salir =  False
-    reloj1 = pygame.time.Clock()
-
-    while not salir:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                salir = True
-        reloj1.tick(20)
-        pygame.display.update()
-    pygame.quit()
+import tkinter
 
 
-main()
